014161ee76c816e4230f759b45584276616c9753
Dragonchu <dr.dragonchu@gmail.com>
2023-12-24 21:16:08 +0800

    树的最大深度

--- algorithms/BinaryTreeTraverse.py
+++ algorithms/BinaryTreeTraverse.py
@@ -14,6 +14,39 @@ def traverse(root):
     # 后序位置
 
 
+def max_depth(root):
+    # 记录最大深度
+    res = 0
+    # 记录遍历到的节点的深度
+    depth = 0
+
+    def traverse(root):
+        nonlocal res
+        nonlocal depth
+        if not root:
+            return
+        # 前序位置
+        depth += 1
+        if not root.left and not root.right:
+            # 到达叶子节点，更新最大深度
+            res = max(res, depth)
+        traverse(root.left)
+        traverse(root.right)
+        # 后序位置
+        depth -= 1
+
+    traverse(root)
+    return res
+
+
+def maxDepth(root):
+    if root is None:
+        return 0
+    left_max = maxDepth(root.left)
+    right_max = maxDepth(root.right)
+    return max(left_max, right_max) + 1
+
+
 if __name__ == '__main__':
     root = Node(1)
     root.left = Node(2)
@@ -23,3 +56,4 @@ if __name__ == '__main__':
     root.right.left = Node(6)
     root.right.right = Node(7)
     traverse(root)
+    print(f"max_depth: {max_depth(root)}, maxDepth: {maxDepth(root)}")
